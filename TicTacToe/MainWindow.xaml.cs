4b710ae42e0c8294159dad91a4db5d530811cc9b
Banko Bnkk <banko.production.network@gmail.com>
2021-01-22 14:14:09 -0500

    38:44

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -20,9 +20,87 @@ namespace TicTacToe
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Private Members
+
+        /// <summary>
+        /// Holds current results of cells in an active game
+        /// </summary>
+        private MarkType[] mResults;
+
+        /// <summary>
+        /// True if it is Player 1's turn (X) or Player 2's turn (X)
+        /// </summary>
+        private bool mPlayer1Turn;
+
+        /// <summary>
+        /// True if game is ended
+        /// </summary>
+        private bool mGameEnded;
+        
+        #endregion
+        
+        #region Constructor
         public MainWindow()
         {
             InitializeComponent();
+
+            NewGame();
+        }
+        #endregion
+        
+        /// <summary>
+        /// Starts new game and resets all values
+        /// </summary>
+        private void NewGame()
+        {
+            // Create blank array of free cells
+            mResults = new MarkType[9];
+
+            for (var i = 0; i < mResults.Length; i++)
+                mResults[i] = MarkType.Free;
+            
+            // Make sure Player 1 starts the game
+            mPlayer1Turn = true;
+
+            // Interact with every button on the grid...
+            Container.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                // Change background, foreground, and content to default values
+                button.Content = string.Empty;
+                button.Background = Brushes.White;
+                button.Foreground = Brushes.Blue;
+            });
+            
+            // Make sure game hasn't ended
+            mGameEnded = false;
+        }
+
+        /// <summary>
+        /// Handles a button click event
+        /// </summary>
+        /// <param name="sender">The button was clicked</param>
+        /// <param name="e">The events of said button</param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Start new game on click after finish
+            if (mGameEnded)
+            {
+                NewGame();
+                return;
+            }
+            
+            // Cast the sender to a button
+            var button = (Button)sender;
+
+            // Find the buttons position in the array
+            var column = Grid.GetColumn(button);
+            var row = Grid.GetRow(button);
+            
+            var index = column + (row * 3);
+
+            // Don't do anything if the cell isn't free
+            if (mResults[index] != MarkType.Free)
+                return;
         }
     }
 }
---
+++ TicTacToe/MarkType.cs
@@ -0,0 +1,21 @@
+﻿namespace TicTacToe
+{
+    /// <summary>
+    /// The current symbol in an area 
+    /// </summary>
+    public enum MarkType
+    {
+        /// <summary>
+        /// The cell hasn't been clicked yet
+        /// </summary>
+        Free,
+        /// <summary>
+        /// The cell has an O in it
+        /// </summary>
+        Circle,
+        /// <summary>
+        /// The cell has an X in it
+        /// </summary>
+        Cross
+    }
+}
